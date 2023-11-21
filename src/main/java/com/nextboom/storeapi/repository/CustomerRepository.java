93f14957cf05bdf35a2d51cecce35b9bb24c21e6
Lemillions <marcosvsdxd@gmail.com>
2023-11-21 09:55:36 -0300

    FEAT: adding annotation Repository to customerRepository

--- src/main/java/com/nextboom/storeapi/repository/CustomerRepository.java
+++ src/main/java/com/nextboom/storeapi/repository/CustomerRepository.java
@@ -3,8 +3,10 @@ package com.nextboom.storeapi.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nextboom.storeapi.entities.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 }
